4a3c75c91ba500d2ca896829e40c66654da02400
EachinVai <usaeachin@gmail.com>
2025-04-26 11:51:56 +0600

    Update ｢File｣__Info.js

--- Script/commands/｢File｣__Info.js
+++ Script/commands/｢File｣__Info.js
@@ -22,18 +22,18 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━━━━━━┓
 ┃      🌟 𝗢𝗪𝗡𝗘𝗥  𝗘𝗔𝗖𝗛𝗜𝗡 🌟      
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : ─꯭─⃝͎̽E̷A̷C̷H̷I̷N̷💋🩶
+┃ 👤 𝐍𝐚𝐦𝐞      : ─꯭─⃝͎̽E̷A̷C̷H̷I̷N̷🤖👊👾
 
 ┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
 ┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝗦𝗶𝗻𝗴𝗶𝗹
-┃ 🎂 𝐀𝐠𝐞       : 16
+┃ 🎂 𝐀𝐠𝐞       : 16+
 ┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
 ┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝗡𝗘𝗪 10
 ┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝗖𝗵𝘂𝗮𝗱𝗮𝗻𝗴𝗮 , 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : eachin34
 ┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : (t.me/eachin443)
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://www.facebook.com/profile.php?id=61575022885586)
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://www.facebook.com/profile.php?id=61575314403790&mibextid=ZbWKwL)
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞: 24/04/2025 11:58:34 PM
 ┗━━━━━━━━━━━━━━━━━━━━━┛
@@ -41,7 +41,7 @@ module.exports.run = async function({ api, event }) {
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    return request(encodeURI(`https://graph.facebook.com/61575022885586/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+    return request(encodeURI(`https://graph.facebook.com/61575314403790/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
         .on('close', () => callback());
 };
